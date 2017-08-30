d14afd9191a22361fb13d5710bc61d7a20c66091
Benjamin <ben639dx@gmail.com>
2017-08-30 23:36:10 +0200

    good install

--- autoinstall.sh
+++ autoinstall.sh
@@ -8,20 +8,20 @@ sudo apt-get install --assume-yes libxext-dev 2>/dev/null
 # Setup your OS
 
 echo "Cloning minilibx library ..."
-cd /tmp
-git clone https://github.com/abouvier/minilibx 2>/dev/null
+sudo cd /tmp
+sudo git clone https://github.com/abouvier/minilibx 2>/dev/null
 echo "Cloning minilibx library ... OK"
 echo "Installing mlx ..."
-mkdir /usr/X11 2>/dev/null
-mkdir /usr/X11/include 2>/dev/null
-mkdir /usr/X11/lib 2>/dev/null
-mkdir /usr/local/lib 2>/dev/null
-cd minilibx 2>/dev/null
+sudo mkdir /usr/X11 2>/dev/null
+sudo mkdir /usr/X11/include 2>/dev/null
+sudo mkdir /usr/X11/lib 2>/dev/null
+sudo mkdir /usr/local/lib 2>/dev/null
+sudo cd minilibx 2>/dev/null
 make 2>/dev/null
-cp mlx.h /usr/X11/include 2>/dev/null
-cp mlx.h /usr/local/include 2>/dev/null
-cp libmlx.a /usr/X11/lib 2>/dev/null
-cp libmlx.a /usr/local/lib 2>/dev/null
+sudo cp mlx.h /usr/X11/include 2>/dev/null
+sudo cp mlx.h /usr/local/include 2>/dev/null
+sudo cp libmlx.a /usr/X11/lib 2>/dev/null
+sudo cp libmlx.a /usr/local/lib 2>/dev/null
 echo "Installing mlx ... OK"
-echo "Do not forget to insert good flags in your Makefile : \n"
+echo "Do not forget to insert good flags in your Makefile :"
 echo "FLAGS = -L/usr/X11/lib /usr/X11/lib/libmlx.a -lXext -lX11"
