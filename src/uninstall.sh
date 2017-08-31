0724b2d967d8815aaa2414f7046e6d11a5121297
Benjamin <ben639dx@gmail.com>
2017-08-31 18:26:44 +0200

    Uninstall try  😱

---
+++ src/uninstall.sh
@@ -0,0 +1,6 @@
+sudo apt --assume-yes remove libxext-dev
+sudo rm -rf /usr/X11/include/mlx.h
+sudo rm -rf /usr/local/include/mlx.h
+sudo rm -rf /usr/X11/lib/libmlx.a
+sudo rm -rf /usr/local/lib/libmlx.a
+sudo rm -rf /tmp/minilibx
