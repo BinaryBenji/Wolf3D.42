e37e8c910b70f7fc91aa22f06aba8fb2640b03c2
Benjamin <ben639dx@gmail.com>
2017-08-31 21:29:14 +0200

    Normally ... 😊

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-
 # Install X11 XShm extension
-
-sudo apt-get install --assume-yes libxext-dev 2>/dev/null
+sudo apt-get install --assume-yes libxext-dev 
 
 # Setup your OS
-
-echo "Cloning minilibx library ..."
-sudo cd /tmp
-sudo git clone https://github.com/abouvier/minilibx 2>/dev/null
-echo "Cloning minilibx library ... OK"
-echo "Installing mlx ..."
+sudo git clone https://github.com/abouvier/minilibx 
 sudo mkdir /usr/X11 2>/dev/null
 sudo mkdir /usr/X11/include 2>/dev/null
 sudo mkdir /usr/X11/lib 2>/dev/null
 sudo mkdir /usr/local/lib 2>/dev/null
-sudo cd minilibx 2>/dev/null
+cd minilibx 2>/dev/null
 make 2>/dev/null
 sudo cp mlx.h /usr/X11/include 2>/dev/null
 sudo cp mlx.h /usr/local/include 2>/dev/null
 sudo cp libmlx.a /usr/X11/lib 2>/dev/null
 sudo cp libmlx.a /usr/local/lib 2>/dev/null
-echo "Installing mlx ... OK"
-echo "Do not forget to insert good flags in your Makefile :"
-echo "FLAGS = -L/usr/X11/lib /usr/X11/lib/libmlx.a -lXext -lX11"
+echo "\nDo not forget to insert good flags in your Makefile :"
+echo "FLAGS = -L/usr/X11/lib /usr/X11/lib/libmlx.a -lXext -lX11\n"
