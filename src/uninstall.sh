08343c18904be95e61068c3a1eab684d75fe8df4
Benjamin <ben639dx@gmail.com>
2017-09-01 15:13:49 +0200

    Cleanup installers ⚔️

--- src/uninstall.sh
+++
@@ -1,6 +0,0 @@
-sudo apt --assume-yes remove libxext-dev
-sudo rm -rf /usr/X11/include/mlx.h
-sudo rm -rf /usr/local/include/mlx.h
-sudo rm -rf /usr/X11/lib/libmlx.a
-sudo rm -rf /usr/local/lib/libmlx.a
-sudo rm -rf /tmp/minilibx
